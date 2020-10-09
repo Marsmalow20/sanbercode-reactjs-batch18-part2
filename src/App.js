5f4e6a86221b12d97c07db17804d1235e183e951
Marsmalow20 <davidrenaldy1@gmail.com>
2020-10-09 13:51:47 +0700

    tugas 9-10 [components]

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
+import Buah from './Tugas-9/Buah';
+import TblHargaBuah from './Tugas-10/TblHargaBuah';
 
 function App() {
   return (
-    <div className="App" style={{margin: '50px auto'}}>
-      <h1 style={{textAlign: 'center'}}>Form Pembelian Buah</h1>
-      <form>
-        <table className="table">
-          <tr>
-            <td width="200"><label style={{padding: '15px', fontWeight: 'bold'}}>Nama Pelanggan</label></td>
-            <td><input type="text" name="nama" style={{padding: '4px', border: '1px solid #aaa', borderRadius: '5px'}}></input></td>
-          </tr>
-          <tr>
-            <td><label style={{padding: '15px', fontWeight: 'bold'}}>Daftar Item</label></td>
-            <td>
-              <input type="checkbox" name="semangka"></input><label>Semangka</label><br></br>
-              <input type="checkbox" name="jeruk"></input><label>Jeruk</label><br></br>
-              <input type="checkbox" name="nanas"></input><label>Nanas</label><br></br>
-              <input type="checkbox" name="salak"></input><label>Salak</label><br></br>
-              <input type="checkbox" name="anggur"></input><label>Anggur</label>
-            </td>
-          </tr>
-          <tr>
-            <td><input type="submit" value="Kirim" style={{padding: '2px auto', borderColor: '#000', borderRadius:'100px'}}></input></td>
-          </tr>
-        </table>
-      </form>
+    <div className="App">
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <Buah />
+      <TblHargaBuah />
 
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
---
+++ src/Tugas-9/Buah.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Buah() {
+    return (
+        <>
+            <div style={{margin: 'auto', border: '1px solid #aaa', borderRadius: '10px', width: '40%', padding: '20px', backgroundColor: '#FFF'}}>
+                <h1 style={{textAlign: 'center'}}>Form Pembelian Buah</h1>
+                <form>
+                <table className="table">
+                    <tr>
+                    <td width="200"><label style={{padding: '15px', fontWeight: 'bold'}}>Nama Pelanggan</label></td>
+                    <td><input type="text" name="nama" style={{padding: '4px', border: '1px solid #aaa', borderRadius: '5px'}}></input></td>
+                    </tr>
+                    <tr>
+                    <td><label style={{padding: '15px', fontWeight: 'bold'}}>Daftar Item</label></td>
+                    <td>
+                        <input type="checkbox" name="semangka"></input><label>Semangka</label><br></br>
+                        <input type="checkbox" name="jeruk"></input><label>Jeruk</label><br></br>
+                        <input type="checkbox" name="nanas"></input><label>Nanas</label><br></br>
+                        <input type="checkbox" name="salak"></input><label>Salak</label><br></br>
+                        <input type="checkbox" name="anggur"></input><label>Anggur</label>
+                    </td>
+                    </tr>
+                    <tr>
+                    <td><input type="submit" value="Kirim" style={{padding: '2px auto', borderColor: '#000', borderRadius:'100px'}}></input></td>
+                    </tr>
+                </table>
+                </form>
+            </div>
+        </>
+    );
+  }
+
+export default Buah
